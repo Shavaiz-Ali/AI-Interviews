fdcf9c1eb72c27e6a0c0d0ca24b3f76727dc173a
Shavaiz-Ali <shavaizali159@gmail.com>
2025-03-24 11:28:52 +0500

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Prepwise",
+  description: "An ai powered platform for preparing mock interview",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
